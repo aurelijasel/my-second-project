31036d6a3ccddc4aa62d230d3646d8c34ad0a6d5
aurelijasel <aurelija.seliukaite@gmail.com>
2025-11-12 21:23:35 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -53,6 +53,12 @@ public:
     friend bool comparePagalVarda(const Studentas& a, const Studentas& b);
     friend bool comparePagalPavarde(const Studentas& a, const Studentas& b);
     friend bool comparePagalEgzamina(const Studentas& a, const Studentas& b);
+
+    friend bool comparePagalVarda(const Studentas& a, const Studentas& b);
+    friend bool comparePagalPavarde(const Studentas& a, const Studentas& b);
+    friend bool comparePagalGalutiniVid(const Studentas& a, const Studentas& b);
+    friend bool comparePagalGalutiniMed(const Studentas& a, const Studentas& b);
 };
 
 #endif
+
