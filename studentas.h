17620c37c0d81e4e5ea43dbad23351200cb1e9a5
aurelijasel <aurelija.seliukaite@gmail.com>
2025-11-12 16:48:29 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -17,11 +17,21 @@ private:
 
 public:
     // --- KONSTRUKTORIAI ---
-    Studentas() : egzaminas_(0), vidurkis_(0), mediana_(0) {}  // default konstruktorius
-    Studentas(const std::string& vard, const std::string& pav) : vardas_(vard), pavarde_(pav), egzaminas_(0), vidurkis_(0), mediana_(0) {}
+    Studentas() : egzaminas_(0), vidurkis_(0), mediana_(0) {}
+
+    Studentas(const std::string& vard, const std::string& pav)
+        : vardas_(vard), pavarde_(pav), egzaminas_(0), vidurkis_(0), mediana_(0) {
+    }
+
+    Studentas(const std::string& vard, const std::string& pav,
+        const std::vector<int>& nd, int egzaminas)
+        : vardas_(vard), pavarde_(pav), nd_(nd), egzaminas_(egzaminas) {
+        skaiciuokVidurkiMediana();
+    }
+
     Studentas(std::istream& is) { readStudent(is); }
 
-    // Getteriai pagal dabartin? kod?
+    // --- GETTERIAI ---
     inline const std::string& getVardas() const { return vardas_; }
     inline const std::string& getPavarde() const { return pavarde_; }
     inline const std::vector<int>& getPazymiai() const { return nd_; }
@@ -29,29 +39,16 @@ public:
     inline double getGalutinisVid() const { return vidurkis_; }
     inline double getGalutinisMed() const { return mediana_; }
 
-    // Setteriai
-    inline void setVardas(const std::string& vard) { vardas_ = vard; }
-    inline void setPavarde(const std::string& pav) { pavarde_ = pav; }
-    inline void setEgzaminas(int egz) { egzaminas_ = egz; }
-    inline void setGalutinisVid(double v) { vidurkis_ = v; }
-    inline void setGalutinisMed(double m) { mediana_ = m; }
     inline void pridetiPazymi(int paz) { nd_.push_back(paz); }
-    inline void setPazymiai(const std::vector<int>& pazymiai) { nd_ = pazymiai; }
-
-    // --- SKAI?IAVIMO FUNKCIJOS ---
-    void skaiciuokVidurkiMediana(); // apskai?iuoja abu rezultatus
-
-    // --- DUOMEN? NUSKAITYMAS ---
+    void skaiciuokVidurkiMediana();
     std::istream& readStudent(std::istream&);
 
-    // --- PAGALBIN?S FUNKCIJOS ---
     static double median(std::vector<int> v);
 
-    // --- PALYGINIMO FUNKCIJOS ---
+    // --- PALYGINIMUI ---
     friend bool comparePagalVarda(const Studentas& a, const Studentas& b);
     friend bool comparePagalPavarde(const Studentas& a, const Studentas& b);
     friend bool comparePagalEgzamina(const Studentas& a, const Studentas& b);
 };
 
 #endif
-
