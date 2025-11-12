78ecf22d40afbfdda62ef400a7874909952b407d
aurelijasel <aurelija.seliukaite@gmail.com>
2025-11-12 16:47:06 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -6,15 +6,19 @@
 std::istream& Studentas::readStudent(std::istream& is) {
     is >> vardas_ >> pavarde_;
     nd_.clear();
+
     int paz;
     std::vector<int> laikini;
     while (is >> paz) {
         laikini.push_back(paz);
     }
+
     if (laikini.empty()) throw std::runtime_error("Nera pazymiu.");
+
     egzaminas_ = laikini.back();
     laikini.pop_back();
     nd_ = laikini;
+
     skaiciuokVidurkiMediana();
     return is;
 }
@@ -25,8 +29,10 @@ void Studentas::skaiciuokVidurkiMediana() {
         mediana_ = egzaminas_ * 0.6;
         return;
     }
+
     double suma = std::accumulate(nd_.begin(), nd_.end(), 0.0);
     double ndvid = suma / nd_.size();
+
     vidurkis_ = egzaminas_ * 0.6 + ndvid * 0.4;
     mediana_ = egzaminas_ * 0.6 + median(nd_) * 0.4;
 }
@@ -41,15 +47,12 @@ double Studentas::median(std::vector<int> v) {
         return v[n / 2];
 }
 
-// --- Palyginimo funkcijos ---
 bool comparePagalVarda(const Studentas& a, const Studentas& b) {
     return a.vardas_ < b.vardas_;
 }
-
 bool comparePagalPavarde(const Studentas& a, const Studentas& b) {
     return a.pavarde_ < b.pavarde_;
 }
-
 bool comparePagalEgzamina(const Studentas& a, const Studentas& b) {
     return a.egzaminas_ < b.egzaminas_;
 }
