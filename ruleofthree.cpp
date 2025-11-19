01cf87975cbd5403278f385902303668f2387419
aurelijasel <aurelija.seliukaite@gmail.com>
2025-11-19 11:31:41 +0200

    Update ruleofthree.cpp

--- ruleofthree.cpp
+++ ruleofthree.cpp
@@ -13,27 +13,31 @@ void demonstruotiruleofthree() {
 
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    // 1. Vartotojo įvestas studentas
-    Studentas s1;
-    cout << "Iveskite studento duomenis (vardas, pavarde, pazymiai ir egzaminas):\n";
-    cin >> s1;
-    cout << "Originalus studentas (s1): " << s1 << endl;
+    {
+        cout << "Iveskite studento duomenis (vardas, pavarde, pazymiai ir egzaminas):\n";
+        Studentas s1;
+        cin >> s1;
+        cout << "Originalus studentas (s1): " << s1 << endl;
 
-    // 2. Kopijavimo konstruktorius
-    Studentas s2 = s1;
-    cout << "Po kopijavimo konstruktoriaus (s2 = s1): " << s2 << endl;
+        // Kopijavimo konstruktorius
+        Studentas s2 = s1;
+        cout << "Po kopijavimo konstruktoriaus (s2 = s1): " << s2 << endl;
 
-    // 3. Priskyrimo operatorius
-    Studentas s3;
-    cout << "Iveskite kita studenta (s3), kuris bus perrasytas s1 duomenimis:\n";
-    cin >> s3;
-    cout << "Pries priskyrima(s3): " << s3 << endl;
+        // Priskyrimo operatorius
+        cout << "Iveskite kita studenta (s3), kuris bus perrasytas s1 duomenimis:\n";
+        Studentas s3;
+        cin >> s3;
+        cout << "Pries priskyrima (s3): " << s3 << endl;
 
-    s3 = s1;
-    cout << "Po priskyrimo operatoriaus (s3 = s1): " << s3 << endl;
+        s3 = s1;
+        cout << "Po priskyrimo operatoriaus (s3 = s1): " << s3 << endl;
 
-    // 4. Savęs priskyrimas
-    s1 = s1;
-    cout << "Po saves priskyrimo (s1 = s1): " << s1 << endl;
+        // Savęs priskyrimas
+        s1 = s1;
+        cout << "Po saves priskyrimo (s1 = s1): " << s1 << endl;
+
+        cout << "Baigiame bloka. Dabar bus kvieciami destruktoriai s1, s2 ir s3.\n";
+    }
+
+    cout << "Blokas baigtas. Visi vartotojo ivesti studentai sunaikinti.\n";
 }
-
